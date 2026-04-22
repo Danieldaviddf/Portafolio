5baa562a814b5b20069a6f32c48bae57c6c80709
Danieldaviddf <danieldaviddf@hotmail.com>
2026-04-22 21:32:39 +0200

    actualizacion fotos

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useReducer } from "react";
-import { appReducer, initialState } from "./appReducer";
+import { AppReducer, initialState } from "./AppReducer";
 
 const AppContext = createContext();
 
